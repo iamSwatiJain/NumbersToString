2d82073579c8e6abf1e37c844eff7387786bdd43
Swati Jain <swati@jain.se>
2018-01-04 13:45:02 +0100

    Add project files.

---
+++ NumbersToString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumToStringCount numberCount = new NumToStringCount();
+            int sum = 0;
+            for (int i=1; i<=1000; i++)
+            {
+                sum+= numberCount.CountLetters(i);
+            }
+
+            Console.WriteLine("The number of letters to write words between one to one thousand are "+sum);
+        }
+    }
+}
